4310fe1fa98ee4f9d1072e1723b8eafc200f78f8
XorgMC <xorgmc@gmail.com>
2014-12-24 10:15:08 +0100

    Version: 4.5 Codename: Gino
    Klassen "5.5", "6.5" und "7.5" hinzugefügt

--- gSApp3/build/generated/source/buildConfig/debug/de/xorg/gsapp/BuildConfig.java
+++ gSApp3/build/generated/source/buildConfig/debug/de/xorg/gsapp/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "de.xorg.gsapp";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 440;
+  public static final int VERSION_CODE = 450;
   public static final String VERSION_NAME = "";
 }
